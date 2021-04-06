59926fa92dc6939e3ff936de5d6891776ee788f1
Georgii Zhulikov <l8doku@gmail.com>
2021-04-06 15:15:11 +0300

    Fix types

--- code/src/problem2_bitwise/main.cpp
+++ code/src/problem2_bitwise/main.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
 
-int getBitAtIndex(int number, int idx)
+int getBitAtIndex(unsigned int number, int idx)
 {
     int bit = 0;
     return bit;
 }
 
 
-int countOnes(int number)
+int countOnes(unsigned int  number)
 {
     int count = 0;
     return count;
 }
 
-void printBinary(int number)
+void printBinary(unsigned int  number)
 {
     
 }
 
 int main()
 {
-    int number = 9;
+    unsigned int  number = 9;
     int idx = 2;
     getBitAtIndex(number, idx);
     countOnes(number);
